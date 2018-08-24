0c5ca2891f2e38e1f3efeb8c8f01e4fbfd30553a
Scott Molsness <smolsness@gmail.com>
2018-08-23 20:59:22 -0700

    created script to make people into a list of lists from the input text file

--- expeditors_code_exercise.py
+++ expeditors_code_exercise.py
@@ -1,14 +1,26 @@
 class Household:
+    def __init__(self, street, city, state, num_occupants):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.num_occupants = num_occupants
 
-    with open('DIT_INPUT_Coding_Excercise.txt', 'r') as f:
-        for line in f:
-            print(line, end = ' ')
+class Person:
 
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, street, city, state, age):
         self.first_name = first_name
         self.last_name = last_name
+        self.street = street
+        self.city = city
+        self.state = state
+        self.age = age
 
-#     def hello(self):
-#         print('Hi, I am', self.first_name, self.last_name)
-#
-# Household('John', 'Smith').hello()
+people = []
+
+with open('DIT_INPUT_Coding_Excercise.txt', 'r') as f:
+
+    for line in f:
+        people.append(line.rstrip().split(','))
+    print(people)
+
+print(people[0][0])
